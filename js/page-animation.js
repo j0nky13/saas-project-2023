bf51b6af73607f189113eb8811a7ec10785e45fc
jonkyle <jonkyle@me.com>
2023-03-09 23:32:41 -0500

    changed stuff

--- js/page-animation.js
+++ js/page-animation.js
@@ -1,3 +1,12 @@
+//  navbar
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
 // animated icons
 
 
